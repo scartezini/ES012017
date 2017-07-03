a4a8d598afc3369d19f6b3663a2aa1c476667e07
Rafael Martins <rafaelmartinsrm@gmail.com>
2017-07-03 03:15:34 -0300

    Restaurantes podem remover apenas suas mesas

--- routes/table.js
+++ routes/table.js
@@ -12,6 +12,7 @@ router.get('/', functions.requireRestaurant, functions.requireLoggedRestaurant, 
             res.status(400)
             res.json(err)
         } else {
+            res.status(200)
             res.json(result.tables)
         }
     })
@@ -54,16 +55,20 @@ router.post('/', functions.requireRestaurant, functions.requireLoggedRestaurant,
 
 	
 //DELETE
-router.delete('/:id',(req,res,next) => {
-	Table.remove({_id : req.params.id},(err) =>{
-		if(err){
-			res.status(400)
-			res.json(err)
-		}
-		else
-			res.json({remove:"ok"});
-
-	})
+router.delete('/:id', functions.requireRestaurant, functions.requireLoggedRestaurant, (req,res,next) => {
+    Restaurant.findOneAndUpdate({_id: req.restaurant._id},
+    {
+        $pull: {tables: req.params.id}
+    },  function(err, result) {
+        if (err) {
+            res.status(400)
+            res.json(err)
+        }
+        else {
+            res.status(200)
+            res.json({result: "Done"})
+        }
+    })
 })
 
 
