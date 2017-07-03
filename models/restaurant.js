fdfbff007a600a3f63fd0b95bf3baa53a6578346
Rafael Martins <rafaelmartinsrm@gmail.com>
2017-07-03 00:43:00 -0300

    Atualiza a model de Restaurant para seguir requerimentos

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,8 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Restaurant = new Schema({
-    email: String,
-    password: String
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    name: String,
+    address: String,
+    number: String,
+    cnpj: String,
+    website: String,
+    tables: [{type: Schema.Types.ObjectId, ref: 'Table'}],
+    menu: {type: Schema.Types.ObjectId, ref: 'Menu'},
 });
 
 
