7d332632fc23f9328695c59fa7b6f6d284845606
Rafael Martins <rafaelmartinsrm@gmail.com>
2017-07-02 21:23:13 -0300

    Implementa sessão para restaurante e página de login

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -4,42 +4,34 @@ var Restaurant = require('../models/restaurant');
 var router = express.Router();
 
 
-router.get('/', function (req, res) {
-    res.render({ user : req.user });
-});
-
-router.get('/register', function(req, res) {
-    res.render('register', { });
-});
-
-router.post('/register', function(req, res) {
-    Restaurant.register(new Restaurant({ email : req.body.email }), req.body.password, function(err, restaurant) {
+router.post('/register', (req, res, next) => {
+    new Restaurant({email: req.body.email, password: req.body.password}).save((err, result) => {
         if (err) {
-            return res.render("register", {info: "Sorry. That username already exists. Try again."})
+            res.status(302)
+            res.json(err)
+        } else {
+            res.status(200)
+            res.json(result._id)
         }
-
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-        });
-    });
+    })
 });
 
-router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
-});
-
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
-});
-
-router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-router.get('/ping', passport.authenticate('local',{section:false}), function(req, res){
-    console.log(req);
-    res.status(200).send("pong!");
-});
+router.post('/login', (req, res, next) => {
+    Restaurant.findOne({email: req.body.email}, (err, result) => {
+        if(!result) {
+            res.status(401)
+            res.json({error: 'Invalid email or password'})
+        } else {
+            if(req.body.password === result.password) {
+                req.session.restaurant = result
+                res.status(200)
+                res.json(result._id)
+            } else {
+                res.status(401)
+                res.json({error: 'Invalid email or password'})
+            }
+        }
+    }) 
+})
 
 module.exports = router;
