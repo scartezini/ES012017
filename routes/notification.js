0acedaccfa81eba56730ed00ac94f0d34deff235
Eduardo Scartezini Correia Carvalho <scartezini@users.noreply.github.com>
2017-07-03 20:28:38 -0300

    Revert "Notificações agora buscam pela sessão da mesa que está enviando"
    This reverts commit 7b0d61cf3b640adb1b5903593fbce331acff142c.

--- routes/notification.js
+++ routes/notification.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 let mongoose = require('mongoose')
 let Notification = require('../models/notification')
-let Table = require('../models/table')
+
 
 //GET
 router.get('/', (req,res,next) => {
@@ -19,30 +19,15 @@ router.get('/', (req,res,next) => {
 
 //POST
 router.post('/',(req,res,next) => {
-    if(req.session && req.session.table) {
-        Table.findOne({_id: req.session.table}, (err, result) => {
-            if (!result) {
-                req.session.destroy();
-                req.status(302);
-                req.json({error: 'Invalid table token'});
-            } 
-            else {
-              new Notification({type: req.body.type, message: req.body.message, table: result}).save((err,notification) => {
-    		        if(err){
-    			        res.status(302)
-    			        res.json(err)
-    		        }
-    		        else{
-    			        res.json(notification)
-    	        	}
-	            })
-            }
-        })
-    }
-    else {
-        res.status(302)
-        res.json({error: 'Invalid table token'})
-    }
+	new Notification(req.body).save((err,notification) => {
+		if(err){
+			res.status(400)
+			res.json(err)
+		}
+		else{
+			res.json(notification)
+		}
+	})
 })
 
 //DELETE 
