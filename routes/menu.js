30aad5f0d04ff4994827ed7a9b60bbf820c2f9e0
Eduardo Scartezini Correia Carvalho <scartezini@users.noreply.github.com>
2017-07-03 20:28:38 -0300

    Revert "Requisições referente ao cardapio"
    This reverts commit 0c53701f580e99f1a040b53a737dc05a02e1e586.

--- routes/menu.js
+++ routes/menu.js
@@ -18,30 +18,4 @@ router.get('/', (req,res,next) => {
 })
 
 
-//POST
-router.post('/',(req,res,next) => {
-	new Menu(req.body).save((err,menu) => {
-		if(err){
-			res.status(400)
-			res.json(err)
-		}else{
-			res.json(menu)
-		}
-	})
-})
-
-
-//PUT
-router.put('/:id',(req,res,next) => {
-	Menu.findOneAndUpdate({ _id: req.params.id }, req.body,
-		{ new: true }, (err, result) => {
-		if(err){
-			res.status(400)
-			res.json(err)
-		}else{
-			res.json(result)
-		}
-	})	
-})
-
 module.exports = router;
